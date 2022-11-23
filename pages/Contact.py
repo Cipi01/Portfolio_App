7b3296d46d14494f15008e6385d8b6fd85b1e3bb
Cipi <cipriangheorghe_capata@yahoo.com>
2022-11-23 17:24:26 +0200

    Send emails using the form

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,10 +1,17 @@
 import streamlit as st
-
+from send_email import send_email
 st.header("Contact me")
 
 with st.form(key="email_form"):
     email = st.text_input("E-mail address")
-    msg = st.text_area("Your message")
+    raw_msg = st.text_area("Your message")
+    msg = f"""\
+Subject: New email from {email}
+
+From: {email}
+{raw_msg}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        print('a')
+        send_email(msg)
+        st.info("Your E-mail has been sent.")
--- send_email.py
+++ send_email.py
@@ -1,19 +1,16 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = 'cipriangheorghecapata@gmail.com'
-password = 'ukqyvxgzcdhafvia'
+def send_email(msg):
+    host = "smtp.gmail.com"
+    port = 465
+    username = 'cipriangheorghecapata@gmail.com'
+    password = 'ukqyvxgzcdhafvia'
+    receiver = 'cipriangheorghecapata@gmail.com'
+    context = ssl.create_default_context()
 
-receiver = 'cipriangheorghecapata@gmail.com'
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, msg)
 
-msg = """\
-Subject: <subject>
-Hi
-Bye!
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, msg)
+
